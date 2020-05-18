04adf2fbbc9f9ba6a3f4450c14d0940995551ba4
imsourceboom <ecmascript.developer@gmail.com>
2020-05-18 13:09:36 +0900

    Test telegram login button widget

--- src/script/telegramBtn/index.js
+++ src/script/telegramBtn/index.js
@@ -3,9 +3,9 @@ const telegramBtn = document.querySelector(
   ".telegram-login-wrapper button.tgme_widget_login_button"
 );
 
-console.log(btnWrapper);
-
-if (telegramBtn !== null) {
-  telegramBtn.textContent = "로그인";
-  telegramBtn.style.cssText = "font-size: 15px;";
-}
+document.addEventListener("DOMContentLoaded", function () {
+  if (telegramBtn !== null) {
+    telegramBtn.textContent = "로그인";
+    telegramBtn.style.cssText = "font-size: 15px;";
+  }
+});
