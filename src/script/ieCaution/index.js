30092916eabd8889160ac363cd65d1cf9429ab65
imsourceboom <ecmascript.developer@gmail.com>
2020-05-18 13:53:17 +0900

    caution wrapper update

--- src/script/ieCaution/index.js
+++ src/script/ieCaution/index.js
@@ -3,6 +3,8 @@ import { browserCheck } from "../agentCheck";
 const cautionWrapper = document.querySelector(".ie-caution-wrapper");
 const browser = browserCheck();
 
-if (browser == "ie") {
-  cautionWrapper.style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", function () {
+  if (browser == "ie") {
+    cautionWrapper.style.display = "block";
+  }
+});
