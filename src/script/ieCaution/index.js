02163af8824e2396244307e556eb0c70680d8b88
imsourceboom <ecmascript.developer@gmail.com>
2020-05-19 16:16:05 +0900

    create ie caution close button and script

--- src/script/ieCaution/index.js
+++ src/script/ieCaution/index.js
@@ -1,12 +1,17 @@
 import { browserCheck } from "../agentCheck";
 
 const cautionWrapper = document.querySelector(".ie-caution-wrapper");
+const cautionClose = document.querySelector(".ie-caution-wrapper img.close");
 const browser = browserCheck();
 
 document.addEventListener("DOMContentLoaded", function () {
   if (browser == "ie") {
     if (cautionWrapper !== null) {
       cautionWrapper.style.display = "block";
+
+      cautionClose.addEventListener("click", function () {
+        cautionWrapper.style.display = "none";
+      });
     }
   }
 });
