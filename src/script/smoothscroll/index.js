347ee701e51285c845a23c12356ac0425a66500c
imsourceboom <ecmascript.developer@gmail.com>
2020-05-20 14:05:03 +0900

    attch move button add to detail page

--- src/script/smoothscroll/index.js
+++ src/script/smoothscroll/index.js
@@ -1,6 +1,16 @@
-import SmoothScroll from 'smooth-scroll';
+import SmoothScroll from "smooth-scroll";
 
-new SmoothScroll('a[href*="#"]', {
+const detailPage = document.querySelector("section#detail");
+
+if (detailPage == null) {
+  new SmoothScroll('a[href*="#"]', {
     speed: 300,
-    speedAsDuration: true
+    speedAsDuration: true,
+  });
+}
+
+new SmoothScroll('a[href="#attach-box"]', {
+  speed: 300,
+  speedAsDuration: true,
+  offset: 100,
 });
