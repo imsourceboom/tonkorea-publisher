27cf658d8e95aa671d49d96656e8a17a61568ef1
imsourceboom <ecmascript.developer@gmail.com>
2020-05-21 15:42:57 +0900

    revise telegram login button align

--- src/script/telegramBtn/index.js
+++ src/script/telegramBtn/index.js
@@ -1,11 +1,28 @@
-// const btnWrapper = document.querySelector(".telegram-login-wrapper");
-// const telegramBtn = document.querySelector(
-//   ".telegram-login-wrapper button.tgme_widget_login_button"
-// );
-
 // document.addEventListener("DOMContentLoaded", function () {
-//   if (telegramBtn !== null) {
-//     telegramBtn.textContent = "로그인";
-//     telegramBtn.style.cssText = "font-size: 15px;";
-//   }
+//   //   const btnComponent = document.querySelector(".telegram-login-component");
+//   //   const btnWrapper = document.querySelector(
+//   //     ".telegram-login-component .profile-wrapper"
+//   //   );
+//   //   btnWrapper.textContent = "로그인";
+//   //   console.log(btnWrapper);
+//   //   var Iframe = document.getElementById("telegram-login-tonkoreabot");
+//   //   var telegramBtn = Array.from(
+//   //     document
+//   //       .getElementById("telegram-login-tonkoreabot")
+//   //       .contentWindow.document.getElementsByClassName("tgme_widget_login_button")
+//   //   );
+//   var iFrameDocument = document.getElementById("telegram-login-tonkoreabot")
+//     .contentWindow.document;
+
+//   iFrameDocument.addEventListener("DOMContentLoaded", function () {
+//     var telegramBtn = iFrameDocument.getElementsByClassName(
+//       "tgme_widget_login_button"
+//     )[0];
+//     console.log(telegramBtn);
+//   });
+
+//   //   telegramBtn.map((btn) => {
+//   //     btn.textContent = "로그인";
+//   //     btn.style.cssText = "color: 15px !important;";
+//   //   });
 // });
