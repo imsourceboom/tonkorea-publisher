f7220e274d3b6326ef8a6c3193688b5e3b41b3fb
imsourceboom <ecmascript.developer@gmail.com>
2020-09-15 10:53:08 +0900

    stop heart event

--- src/script/postBox/index.js
+++ src/script/postBox/index.js
@@ -15,8 +15,26 @@ if (contentsBox !== null) {
     document.querySelectorAll(".user-thumbnail")
   );
   const userName = Array.from(document.querySelectorAll(".username"));
-  const heartButtons = Array.from(document.querySelectorAll("button.heart"));
+  // const heartButtons = Array.from(document.querySelectorAll("button.heart"));
   const linkButton = document.querySelector("button.link");
+  const commentBox = Array.from(document.querySelectorAll(".comment-box"));
+  const replyMoreButtons = Array.from(
+    document.querySelectorAll(".re-reply-toggle-btn")
+  );
+  const replyFormTriggerButtons = Array.from(
+    document.querySelectorAll(".re-reply-form-trigger")
+  );
+  const reReplyWrap = Array.from(
+    document.querySelectorAll(".re-reply-comment-wrap")
+  );
+  const replyForm = Array.from(
+    document.querySelectorAll(".comment-box .write-component-wrap")
+  );
+  const replyFormCancelButtons = Array.from(
+    document.querySelectorAll(
+      ".comment-box .write-component-wrap button.cancel-btn"
+    )
+  );
 
   userThumbnail.map((thumbnail, thumbnailIndex) => {
     userName.map((name, nameIndex) => {
@@ -27,11 +45,11 @@ if (contentsBox !== null) {
     });
   });
 
-  heartButtons.map((btn) => {
-    btn.addEventListener("click", (e) => {
-      e.currentTarget.classList.toggle("clicked");
-    });
-  });
+  // heartButtons.map((btn) => {
+  //   btn.addEventListener("click", (e) => {
+  //     e.currentTarget.classList.toggle("clicked");
+  //   });
+  // });
 
   const link = new Clipboard(linkButton);
   linkButton.dataset.clipboardText = `${window.location.origin}${window.location.pathname}`;
@@ -45,4 +63,39 @@ if (contentsBox !== null) {
   };
 
   link.on("success", tooltips);
+
+  if (commentBox !== null) {
+    if (replyMoreButtons !== null) {
+      replyMoreButtons.map((btn, i) => {
+        btn.addEventListener("click", (e) => {
+          reReplyWrap.map((wrap, j) => {
+            if (i === j) {
+              btn.classList.toggle("active");
+              wrap.classList.toggle("active");
+            }
+          });
+        });
+      });
+    }
+
+    replyFormTriggerButtons.map((btn, i) => {
+      btn.addEventListener("click", () => {
+        replyForm.map((form, j) => {
+          if (i === j) {
+            form.classList.add("active");
+          }
+        });
+      });
+    });
+
+    replyFormCancelButtons.map((btn, i) => {
+      btn.addEventListener("click", () => {
+        replyForm.map((form, j) => {
+          if (i === j) {
+            form.classList.remove("active");
+          }
+        });
+      });
+    });
+  }
 }
